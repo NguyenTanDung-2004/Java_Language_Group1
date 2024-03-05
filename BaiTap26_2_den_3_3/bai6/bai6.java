3ee8ba2584a7cf655bd8ebd12af655a48966d5e1
GiangPhanNguyenTra <146115355+GiangPhanNguyenTra@users.noreply.github.com>
2024-03-05 17:09:28 +0700

    Update bai6.java

--- BaiTap26_2_den_3_3/bai6/bai6.java
+++ BaiTap26_2_den_3_3/bai6/bai6.java
@@ -17,6 +17,7 @@ public class bai6 {
     public static int getNumberOfDaysInMonth(int y, int m) {
         if (isLeapYear(y) && m == 2) 
             return 29;
+        else if (m == 2) return 28;
         if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
             return 31;
         return 30;

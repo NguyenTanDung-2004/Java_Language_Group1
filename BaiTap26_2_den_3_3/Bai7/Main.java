218f85a2032f4a386577a8eaaf6b9f879b852ea4
Vhquynhanh <146148895+Vhquynhanh@users.noreply.github.com>
2024-03-06 11:12:25 +0700

    Update bài 7

--- BaiTap26_2_den_3_3/Bai7/Main.java
+++ BaiTap26_2_den_3_3/Bai7/Main.java
@@ -1,8 +1,5 @@
-package MangPhanSo;
-
 import java.util.Scanner;
-
-public class Main {
+public class bai7 {
 
 	public static void main(String[] args) {
 		
@@ -46,4 +43,41 @@ public class Main {
 		
 	}
 
+}
+class PhanSo {
+
+	private int tu;
+	private int mau;
+	
+	PhanSo()
+	{
+		tu=0;
+		mau=1;
+	}
+	
+	PhanSo(int tu, int mau)
+	{
+		this.tu=tu;
+		this.mau=mau;
+	}
+	public double getGiaTri()
+	{
+		return (double) this.tu/this.mau;
+	}
+	public void Nhap()
+	{
+		Scanner scanner = new Scanner(System.in);
+		String ps = scanner.next();
+		String split[] = ps.split("/");
+		
+		if(split.length == 2)
+		{
+			this.tu = Integer.parseInt(split[0]);
+			this.mau = Integer.parseInt(split[1]);
+		}
+	}
+	public String toString()
+	{
+		return String.valueOf(this.tu) + "/" + String.valueOf(this.mau);
+	}
 }
